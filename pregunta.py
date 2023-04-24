693c4f08d56ac1f516ad9f02232dd0542db06c3c
unknown <seochoac@unal.edu.co>
2023-04-23 22:26:11 -0500

    adelanto

--- pregunta.py
+++ pregunta.py
@@ -13,8 +13,30 @@ def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
+    df.dropna(axis=0, inplace=True)
+
+    df['sexo']=df['sexo'].str.lower()
+
+    df['tipo_de_emprendimiento']=df['tipo_de_emprendimiento'].str.lower()
+
+    df['idea_negocio']=df['idea_negocio'].str.lower()
+    df['idea_negocio']=df['idea_negocio'].replace('-', ' ')
+    df['idea_negocio']=df['idea_negocio'].replace('_', ' ')
+
+    df['barrio']=df['barrio'].str.lower()
+    df['barrio']=df['barrio'].replace('-', ' ')
+    df['barrio']=df['barrio'].replace('_', ' ')
+
+    df['monto_del_credito']=df['monto_del_credito'].replace('.','')
+    df['monto_del_credito']=df['monto_del_credito'].replace(',','')
+    df['monto_del_credito']=df['monto_del_credito'].str.strip('$')
+
+    df.drop_duplicates(inplace=True)
+
+    print(df['sexo'].value_counts())
     #
     # Inserte su código aquí
     #
 
     return df
+print(clean_data())
