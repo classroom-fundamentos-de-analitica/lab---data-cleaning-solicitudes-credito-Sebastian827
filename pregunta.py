46b3208148669a58c60afd45279970adcb4b6cc9
unknown <seochoac@unal.edu.co>
2023-06-08 16:16:22 -0500

    prueba

--- pregunta.py
+++ pregunta.py
@@ -14,46 +14,52 @@ def clean_data():
     df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col = 0)
 
     
+    
     df.dropna(axis=0, inplace=True)
 
     
     df['sexo'] = df['sexo'].str.lower()
     df['sexo'] = df['sexo'].str.replace('_', ' ')
     df['sexo'] = df['sexo'].str.replace('-', ' ')
+    
 
     df['tipo_de_emprendimiento']=df['tipo_de_emprendimiento'].str.lower()
     df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.replace('_', ' ')
     df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.replace('-', ' ')
 
     df['idea_negocio']=df['idea_negocio'].str.lower()
-    df['idea_negocio']=df['idea_negocio'].replace('-', ' ')
-    df['idea_negocio']=df['idea_negocio'].replace('_', ' ')
+    df['idea_negocio']=df['idea_negocio'].str.replace('-', ' ')
+    df['idea_negocio']=df['idea_negocio'].str.replace('_', ' ')
 
     df['línea_credito'] = df['línea_credito'].str.lower()
     df['línea_credito'] = df['línea_credito'].str.replace('_', ' ')
     df['línea_credito'] = df['línea_credito'].str.replace('-', ' ')
 
     df['barrio']=df['barrio'].str.lower()
-    df['barrio']=df['barrio'].replace('-', ' ')
-    df['barrio']=df['barrio'].replace('_', ' ')
-
-    df['comuna_ciudadano']=df['comuna_ciudadano'].astype(str).str.strip('.0')
-
-    df['fecha_de_beneficio']=pd.to_datetime(df['fecha_de_beneficio'],format='%d/%m/%Y' , errors='coerce').fillna(
-            pd.to_datetime(df['fecha_de_beneficio'], format='%Y/%m/%d', errors='coerce'))
+    df['barrio']=df['barrio'].str.replace('-', ' ')
+    df['barrio']=df['barrio'].str.replace('_', ' ')
     
-    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].dt.strftime('%d-%m-%Y')
+    df['estrato'] = df['estrato'].astype(int)
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(int)
 
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True)
+    
+
+    
+    
     df['monto_del_credito']=df['monto_del_credito'].str.strip('$')
     df['monto_del_credito']=df['monto_del_credito'].str.replace(",","")
     df['monto_del_credito']=df['monto_del_credito'].astype(float)
     df['monto_del_credito']=df['monto_del_credito'].astype(int)
+    
 
 
-    df['línea_credito']=df['línea_credito'].str.lower()
-    df=df.dropna(inplace=True)
+   
+  
+    df.drop_duplicates(inplace=True)
 
-    #print(df['sexo'].value_counts())
+
+    print(df['tipo_de_emprendimiento'].value_counts().to_list())
     #print(df['línea_credito'].isnull().sum())
     #
     # Inserte su código aquí
